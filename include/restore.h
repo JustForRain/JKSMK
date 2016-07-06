c6bc2133bd1418b593c7b5f7487dca9b9766aacf
ShinyMK <shinymagikarp@outlook.com>
2016-07-06 20:34:46 +0100

    1.0.8

---
+++ include/restore.h
@@ -0,0 +1,9 @@
+#ifndef RESTORE_H
+#define RESTORE_H
+
+#include "titledata.h"
+
+bool restoreData(const titleData dat, FS_Archive arch, int mode);
+bool restoreDataSDPath(const titleData dat, FS_Archive arch, int mode);
+
+#endif // RESTORE_H
